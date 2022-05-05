9cce5ddeea33ff1d032c470313eca24799269b09
ionetty <74424759+ionetty@users.noreply.github.com>
2022-05-04 23:23:50 -0700

    fix auto lightspeed uv catagorization

--- static/prx/prx.config.js
+++ static/prx/prx.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: '/bare/', // change this to your bare server if you want to use it
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: '/prx/prx.handler.js',
+    bundle: '/prx/prx.bundle.js',
+    config: '/prx/prx.config.js',
+    sw: '/prx/prx.sw.js',
 };
